e23fe42201fb02399e6ac805d9dbf3774d08c8ad
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-03 07:18:53 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,25 @@
+var num1 = '33';
+var num2 = 44;
+num1 = parseInt(num1);
+console.log(num1 + num2);
+// var num3 = 55.95;
+// var num4 = 66;
+// var num5 = 77.25;
+// var totalCost = num1 + num2 + num3 + num4 + num5;
+// var avg = totalCost % 5;
+// avg = avg.toFixed(2);
+// console.log(avg);
+// var converting = parseFloat(avg);
+// console.log(converting);
+/*
+-type of variable
+-Primitive data types
+-neumaric
+-string
+-boolean
+-undifiend
+-null
+-Non primitive data types
+-object
+
+*/
