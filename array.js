1f3adb434809d22b9aabef7fa3831c16e1a6a8f5
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-03 09:35:49 +0600

    add

---
+++ array.js
@@ -0,0 +1,31 @@
+// var friends = ['sabbir', 'tushar', 'akib', 'foysal', 'antor', 'raihan'];
+// friends[0] = 'ratul';
+// friends[2] = 'ismail';
+// // console.log(friends);
+// var positionIndex = friends.indexOf('foysal');
+// console.log(positionIndex);
+// var nums =[1, 5, 7];
+// nums.push('ratul');
+// console.log(nums);
+// nums.pop();
+// console.log(nums);
+// nums.shift();
+// console.log(nums);
+// nums.unshift('Hello');
+// console.log(nums);
+var myMoney = 10000;
+if(myMoney >= 80000){
+    console.log('I can buy Mac');
+}
+else if(myMoney >= 60000){
+    console.log('I can buy Gamming Laptop');
+}
+else if(myMoney >= 40000){
+    console.log('I can buy Lenovo Yoga');
+}
+else if(myMoney >= 20000){
+    console.log('I need to buy old Laptop');
+}
+else{
+    console.log('I need to use my phone');
+}
---
+++ loops.js
@@ -0,0 +1,43 @@
+// for(i = 58; i <=98; i++){
+//     console.log(i);
+// }
+// even 
+// for(i = 412; i < 623; i +=2){
+//     console.log(i);
+// }
+// odd 
+// for (i = 581; i <= 623; i +=2){
+//     console.log(i);
+// }
+// var developer = ['html', 'css', 'bootstrap', 'tailwind', 'github', 'netlify'];
+
+// for(var i = 0; i < 6; i++){
+//     var item = developer[i];
+//     console.log(item);
+// }
+
+
+// var model = ['nokia', 'tablet', 'samsung', 'samphony', 'realme'];
+// var i = 0;
+// while(i < 5){
+//     var mobile = model[i];
+//     console.log(mobile);
+//     i++;
+// }
+
+// for(i = 30; i <=86; i++){
+//     // console.log(i);
+//     if(i >=44){
+//         break;
+// //     }
+// }
+var books = [120, 136, 180, 198, 250, 220, 230];
+var i =0;
+while(i < 7){
+    var num = books[i];
+    console.log(num);
+    i++;
+    if(i > 200){
+        break;
+    }
+}
